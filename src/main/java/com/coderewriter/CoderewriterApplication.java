ada909932c3d647a04f41f1079f06fd2718bfd89
Vamshi_3103 <vkovvuri@stevens.edu>
2026-06-22 15:48:13 -0400

    first commit

---
+++ src/main/java/com/coderewriter/CoderewriterApplication.java
@@ -0,0 +1,13 @@
+package com.coderewriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderewriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderewriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderewriter/CoderewriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderewriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderewriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
